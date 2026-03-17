0040217aeb53c772977c896634c246f876dcc249
Daniel Mora <djosemora@ucundinamarca.edu.co>
2026-03-17 17:56:00 -0500

    ServerRoutes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,24 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
+   {
+    path: 'pokemons',
+    renderMode: RenderMode.Prerender
   },
+  {
+    path: 'pokemon/:id',
+    renderMode: RenderMode.Server 
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'pricing',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  }
 ];
